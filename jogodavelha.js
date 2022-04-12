14ba929f9bee8b141740a9cc9e5b9faef93595e6
TheDuit <53624486+TheDuit@users.noreply.github.com>
2022-04-11 23:18:26 -0300

    corrigindo commit ii

--- jogodavelha.js
+++ jogodavelha.js
@@ -4,7 +4,7 @@ var playerLabel = document.getElementById("jogador")
 function squareChoser(evt){
     if((player % 2) == 0){
         playerLabel.innerText="Jogador 02";
-        evt.currentTarget.style.background="#BF3C30";
+        evt.currentTarget.style.background="#BF3C30"; //currentTarget pega o elemento no qual o evento foi disparado, nao precisa mandar o elemento como argumento na chamada da função
         player++;
     }
     else{
@@ -15,9 +15,9 @@ function squareChoser(evt){
     
 }
 
-var squares=window.document.getElementsByClassName('square'); //retorna uma coleção com os elementos da classe 
-for (i=0; i < squares.length; i++){
-    squares[i].addEventListener('click', squareChoser)
+var squares=window.document.getElementsByClassName('square'); //retorna uma coleção com os elementos da classe square
+for (i=0; i < squares.length; i++){ //um for para percorrer os elementos da coleção 
+    squares[i].addEventListener('click', squareChoser) //adicionando eventlisteners nos elementos da coleção
 }
 //var square =new Map(squares , addEventListener('click', squareChoser));//cast da coleção em um objeto Map, para poder usar o foreach
 
